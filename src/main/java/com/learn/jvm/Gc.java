7422c266dc33a18b8511a98b91a1b42c2ee5b872
xiaofan <xiaofan@tsign.cn>
2019-05-28 16:28:20 +0800

    jvm

---
+++ src/main/java/com/learn/jvm/Gc.java
@@ -0,0 +1,5 @@
+package com.learn.jvm;
+
+
+public class Gc {
+}
---
+++ src/main/java/com/learn/jvm/MemErrorTest.java
@@ -0,0 +1,30 @@
+package com.learn.jvm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 模拟内存溢出：
+ */
+public class MemErrorTest {
+    public static void main(String[] args) {
+        try {
+            List<Object> list = new ArrayList<>();
+            for (; ; ) {
+                list.add(new Object());//创建对象速度可能高于jvm回收速度
+            }
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+        }
+        try {
+            hi();//递归造成StackOverflowError 这边因为每运行一个方法将创建一个栈帧，栈帧创建太多无法继续申请到内存扩展
+        } catch (StackOverflowError e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void hi() {
+        hi();
+    }
+
+}
